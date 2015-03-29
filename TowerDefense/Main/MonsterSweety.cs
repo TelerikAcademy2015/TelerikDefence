6b3aeb9093f8650072f799e8a6fa98621163b095
Stivi69 <Stivi69@gmail.com>
2015-03-29 23:42:20 +0300

    Create MonsterSweety.cs

---
+++ TowerDefense/Main/MonsterSweety.cs
@@ -0,0 +1,32 @@
+//Monster Sweety - average speed monster with with average helth.
+namespace TowerDefense.Main
+{
+    using System;
+    using System.Linq;
+    using TowerDefense.Interfaces;
+    using System.Windows.Media;
+    using TowerDefense.Utils;
+
+
+    class MonsterSweety : Monster//, IMonster, IGameObject, IMovable, ITarget - Commented because of team rule.
+    {
+        public MonsterSweety(IRoute route)
+            : base(route, 60, 25)
+        {
+            this.IsDestroyed = false;
+            this.ChangeImage();
+        }
+        public override ImageSource ImageSource
+        {
+            get
+            {
+                return ImageFactory.CreateImage("MonsterSweety.png");
+            }
+        }
+
+        public void ChangeImage()
+        {
+
+        }
+    }
+}
