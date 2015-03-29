d02efa8d40844592bf8e84d88e8c91bf713ec9c7
Stivi69 <Stivi69@gmail.com>
2015-03-29 23:42:50 +0300

    Create MonsterGirl.cs

---
+++ TowerDefense/Main/MonsterGirl.cs
@@ -0,0 +1,32 @@
+//Girl Monster - fast monster with low health.
+namespace TowerDefense.Main
+{
+    using System;
+    using System.Linq;
+    using TowerDefense.Interfaces;
+    using System.Windows.Media;
+    using TowerDefense.Utils;
+
+
+    class MonsterGirl : Monster//, IMonster, IGameObject, IMovable, ITarget - Commented because of team rule.
+    {
+        public MonsterGirl(IRoute route)
+            : base(route, 100, 10)
+        {
+            this.IsDestroyed = false;
+            this.ChangeImage();
+        }
+        public override ImageSource ImageSource
+        {
+            get
+            {
+                return ImageFactory.CreateImage("MonsterGirl.png");
+            }
+        }
+
+        public void ChangeImage()
+        {
+
+        }
+    }
+}
