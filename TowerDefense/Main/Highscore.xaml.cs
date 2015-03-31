3254836f8c10b8ef956d7d9651d6092e33c6521a
Manov <georgimanov@gmail.com>
2015-03-31 17:02:32 +0300

    Added Highscore Page (not implemented yet)

--- TowerDefense/Main/Highscore.xaml.cs
+++ TowerDefense/Main/Highscore.xaml.cs
@@ -1,12 +1,30 @@
-﻿using TowerDefense.WPFCustomControls;
-
-namespace TowerDefense.Main
+﻿namespace TowerDefense.Main
 {
+    using System;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+    using TowerDefense.WPFCustomControls;
     public partial class Highscore : PropertyChangedAwaredPage
     {
         public Highscore()
         {
             InitializeComponent();
+            this.OpenMainMenuPage = new DelegateCommand((Object parameter) =>
+            {
+                this.OpenPage(new MainMenu());
+            });
+            this.DataContext = this;
+        }
+        public ICommand OpenMainMenuPage
+        {
+            get;
+            private set;
+        }
+
+        private void OpenPage(Page page)
+        {
+            ((Window)this.Parent).Content = page;
         }
     }
 }
