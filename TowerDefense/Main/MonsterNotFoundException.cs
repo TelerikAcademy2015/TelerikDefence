4534e5d624cdd0bbb0a2c44bea562a4c07c8e018
Manov <georgimanov@gmail.com>
2015-03-31 13:12:20 +0300

    Added:  Custom exception class

---
+++ TowerDefense/Main/MonsterNotFoundException.cs
@@ -0,0 +1,21 @@
+﻿namespace TowerDefense.Main
+{
+    using System;
+
+    public class CreepNotFoundException : Exception
+    {
+        public CreepNotFoundException()
+        {
+        }
+
+        public CreepNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public CreepNotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
