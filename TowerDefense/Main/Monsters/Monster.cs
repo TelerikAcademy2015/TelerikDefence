4746c0123e193b65cf269ebce4958c6f9253920d
Stivi69 <Stivi69@gmail.com>
2015-04-03 02:49:51 +0300

    Monster Position
    Trying to get monsters exactly on the way.

--- TowerDefense/Main/Monsters/Monster.cs
+++ TowerDefense/Main/Monsters/Monster.cs
@@ -10,6 +10,7 @@
     public abstract class Monster : GameObject, IMonster
     {
         private List<string> imageFiles = new List<string>();
+        private Point position = new Point();
         private int fileNum = 0;
 
         public int Speed
@@ -46,6 +47,19 @@
         {
             get;
             private set;
+        }
+
+        public Point Position
+        {
+            get { return this.position; }
+            set
+            {
+                if (this.position.X == 0 && this.position.Y == 0)
+                {
+                    value = this.Route.Points.Select(p => p = new Point(p.X - this.ImageSource.Width, p.Y - this.ImageSource.Height)).First();
+                }
+                this.position = new Point(value.X - this.ImageSource.Width / this.ImageSource.Width, value.Y - this.ImageSource.Height / this.ImageSource.Width);
+            }
         }
 
         public List<string> ImageFIles
