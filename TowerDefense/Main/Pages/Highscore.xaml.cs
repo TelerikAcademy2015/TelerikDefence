41fe4614fa01003815b7f4728c17b738b223ccdc
georgi <georgimanov@gmail.com>
2015-04-07 17:41:54 +0300

    Highscore = provide sample info to Highscore page

--- TowerDefense/Main/Pages/Highscore.xaml.cs
+++ TowerDefense/Main/Pages/Highscore.xaml.cs
@@ -1,29 +1,31 @@
-﻿namespace TowerDefense.Main
+﻿
+using System.Collections.Generic;
+
+namespace TowerDefense.Main
 {
     using System;
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Input;
+    using System.Windows.Media;
+
     using TowerDefense.WPFCustomControls;
     public partial class Highscore : PropertyChangedAwaredPage
     {
-       
-
-
         public Highscore()
         {
-             
-
+            InitializeComponent();
+            
+            
             this.OpenMainMenuPage = new DelegateCommand((Object parameter) =>
             {
                 this.OpenPage(new MainMenu());
             });
 
+            this.PrintResults();
             this.DataContext = this;
-            InitializeComponent();
-
-              
         }
+
         public ICommand OpenMainMenuPage
         {
             get;
@@ -38,11 +40,36 @@
         private void PrintHighscore()
         {
             var highscores = ApplicationContext.Instance.HighscoreProvider.HighscoreEntries;
-            foreach (var highscore in highscores)
+        }
+
+        private void PrintResults()
+        {
+            var results = GetResults();
+
+            var i = 0;
+
+            foreach (var result in results)
             {
-                this.Content = highscore.Name;
-                //PRINT TO PAGE"{0, -15}{1, 10}", entry.Name, entry.Score);
+                TextBlock HelloWorldTextBlock = new TextBlock();
+                HelloWorldTextBlock.FontSize = 24;
+
+                HelloWorldTextBlock.Text = "[" + (i + 1) + "]" + result.Key + ": " + result.Value;
+
+                LayoutGrid.Children.Add(HelloWorldTextBlock);
+                Grid.SetRow(HelloWorldTextBlock, i++);
+                Grid.SetColumn(HelloWorldTextBlock, 2);
             }
         }
+
+        private Dictionary<string, int> GetResults()
+        {
+            var results = new Dictionary<string, int>();
+
+            results.Add("Pesho", 100);
+            results.Add("Gosho", 200);
+            results.Add("Tosho", 300);
+
+            return results;
+        }  
     }
 }
